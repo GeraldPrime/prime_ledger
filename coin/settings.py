dfc22baaa2e4c4bd0f697c98b78946f851ba0e65
Gerald Prime <geraldokeke68@gmail.com>
2025-02-02 20:12:22 +0100

    vercel ignore

--- coin/settings.py
+++ coin/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 #     BASE_DIR/'static',
 # ]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
